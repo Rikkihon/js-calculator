51fea36c8cfb9fea026c67622c580bab1cb24725
Rikki Honnold <rikki@er-10-132-240-32.wireless.umn.edu>
2018-03-06 21:24:14 -0600

    5 min left in class

---
+++ calc.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const button;
+$( ".number" ).click(function() {
+    // alert( "Handler for .click() called." );
+    //append the value to the h1 with the id 
+    $('#first-number').text(value);
+console.log();
+  });
